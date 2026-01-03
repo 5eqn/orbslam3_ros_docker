54be2ac9a83bfd2edcf060e840fbf3d59fea64ba
5eqn <subcat2077@gmail.com>
2026-01-03 11:25:16 +0800

    Add usage and docker compose

---
+++ README.md
@@ -0,0 +1,8 @@
+# ORB-SLAM3 ROS Docker
+
+Usage: 
+
+```sh
+./build.sh
+docker compose up
+```
--- run_kalibr.sh
+++
@@ -1,2 +0,0 @@
-xhost +local:root
-docker run -it -e "DISPLAY" -e "QT_X11_NO_MITSHM=1" -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" -v "~/foo:/foo" stereolabs/kalibr:kinetic
